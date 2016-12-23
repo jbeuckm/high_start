f31e621259e805a17f469d51f43fde74d8b6862e
Joe Beuckman <joebeuckman@gmail.com>
2016-12-22 22:01:53 -0600

    logging acc and gyros to sd card

--- high_start_atmega/high_start_atmega/high_start_atmega.ino
+++ high_start_atmega/high_start_atmega/high_start_atmega.ino
@@ -1,6 +1,8 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include <Servo.h>
+#include <SPI.h>
+#include <SD.h>
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
@@ -11,13 +13,17 @@ MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
-
 Servo myservo;
 int pos = 0;
 int dir = 1;
 
+const int chipSelect = 13;
+File dataFile;
+
 void setup() {
-  
+  Serial.begin(38400);
+
+
   // join I2C bus (I2Cdev library doesn't do this automatically)
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -26,8 +32,6 @@ void setup() {
         Serial.println("Fastwire");
   #endif
 
-  Serial.begin(38400);
-
   Serial.println("Initializing I2C devices...");
   accelgyro.initialize();
 
@@ -35,20 +39,57 @@ void setup() {
   Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
   myservo.attach(9);
+
+
+  Serial.print("Initializing SD card...");
+  pinMode(SS, OUTPUT);
+  
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1) ;
+  }
+  Serial.println("card initialized.");
+  
+  dataFile = SD.open("datalog.tsv", FILE_WRITE);
+  if (! dataFile) {
+    Serial.println("error opening datalog.txt");
+    // Wait forever since we cant write data
+    while (1) ;
+  }
+
+      String dataString = "millis\t";
+
+  dataString += "ax\t"; 
+  dataString += "ay\t"; 
+  dataString += "az\t"; 
+  dataString += "gx\t"; 
+  dataString += "gy\t"; 
+  dataString += "gz\t"; 
+
+  dataFile.println(dataString);
+        Serial.println(dataString);
 }
 
 void loop() {
 
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    Serial.print("a/g:\t");
-    Serial.print(ax); Serial.print("\t");
-    Serial.print(ay); Serial.print("\t");
-    Serial.print(az); Serial.print("\t");
-    Serial.print(gx); Serial.print("\t");
-    Serial.print(gy); Serial.print("\t");
-    Serial.println(gz);
+  String dataString = String(millis()) + "\t";
 
+  dataString += String(ax) + "\t"; 
+  dataString += String(ay) + "\t"; 
+  dataString += String(az) + "\t"; 
+  dataString += String(gx) + "\t"; 
+  dataString += String(gy) + "\t"; 
+  dataString += String(gz) + "\t"; 
+
+  dataFile.println(dataString);
+
+  // print to the serial port too:
+  Serial.println(dataString);
+
+  
     myservo.write(ay >> 7);
     pos += dir;
     if ((dir == 1) && (pos >= 180)) dir = -1;
